b7f4f13a9202ad618b46210c9dbdb52e432040e4
dubstitch <desireewade5@gmail.com>
2026-05-05 11:54:49 -0400

    Load homepage program dropdown replacement

---
+++ js/home-programs-loader.js
@@ -0,0 +1,6 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'js/home-programs-fix.js?v=' + Date.now();
+  script.defer = true;
+  document.head.appendChild(script);
+})();
